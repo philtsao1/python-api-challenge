97883286aece30e21ab9b1b1197537bc78a9c9a9
Phil Tsao <34148122+philtsao1@users.noreply.github.com>
2023-12-19 23:06:06 -0500

    Update api_keys.py

--- starter_code/api_keys.py
+++ starter_code/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "caf11ff39f1dd57ec0017fbe703a04d4"
+weather_api_key = "YOUR API KEY HERE"
 
 # Geoapify API Key
-geoapify_key = "aa59900724c745ecaa55794b98e8b264"
+geoapify_key = "YOUR API KEY HERE"
